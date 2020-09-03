5b964a8fcd41abd1c22968938e78b9cc9d56a2d0
Tanner Lambert <lambert.tanner23@gmail.com>
2020-09-02 20:18:15 -0600

    ..

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Password",
+  password: "secretpassword",
   database: "burgers2.0_db"
 });
 if (process.env.JAWSDB_URL) {
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
   connection: mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Password",
+    password: "secretpassword",
     database: "burgers2.0_db"
   })
 }
